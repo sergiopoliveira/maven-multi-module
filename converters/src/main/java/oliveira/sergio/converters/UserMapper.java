540a0a6431b09cf34dd1155a5a67094e47f9fa15
sergiopoliveira <sergiopoliveira@outlook.com>
2019-02-12 21:59:21 +0000

    MapStruct Module

--- converters/src/main/java/oliveira/sergio/converters/UserMapper.java
+++ converters/src/main/java/oliveira/sergio/converters/UserMapper.java
@@ -1,4 +1,16 @@
 package oliveira.sergio.converters;
 
+import entities.User;
+import guru.springframework.domain.UserCommand;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserCommand userToUserCommand(User user);
+
+    User userCommandToUser(UserCommand userCommand);
 }
--- web-app/src/main/java/oliveira/sergio/controllers/UserController.java
+++ web-app/src/main/java/oliveira/sergio/controllers/UserController.java
@@ -2,12 +2,13 @@ package oliveira.sergio.controllers;
 
 import entities.User;
 import guru.springframework.domain.UserCommand;
+import oliveira.sergio.converters.UserMapper;
 
 public class UserController {
 
     User saveUser(UserCommand command){
 
         //fake impl
-        return new User();
+        return UserMapper.INSTANCE.userCommandToUser(command);
     }
 }
